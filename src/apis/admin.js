cc2847964ef8b7acb66f3c350f56037897fb173f
rachellam <rachellam@tuofaninfo.com>
2022-02-16 12:40:52 +0800

    admin restaurant edit: connected to api

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -20,7 +20,17 @@ export default {
         headers: { Authorization: `Bearer ${getToken()}` },
       });
     },
-    delete({restaurantId}) {
+    getDetail({ restaurantId }) {
+      return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
+        headers: { Authorization: `Bearer ${getToken()}` },
+      });
+    },
+    update ({ restaurantId, formData }) {
+      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, {
+        headers: { Authorization: `Bearer ${getToken()}` },
+      });
+    },
+    delete({ restaurantId }) {
       return apiHelper.delete(`/admin/restaurants/${restaurantId}`, {
         headers: { Authorization: `Bearer ${getToken()}` },
       });
--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -1,82 +1,104 @@
 <template>
- <div class="container py-5">
+  <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
     <AdminRestaurantForm
       :initial-restaurant="restaurant"
-      @after-submit="handleAfterSubmit" 
+      :is-processing="isProcessing"
+      @after-submit="handleAfterSubmit"
     />
   </div>
 </template>
 
 <script>
-import AdminRestaurantForm from "../components/AdminRestaurantForm.vue"
-
-const dummyData = {
-  'restaurant': {
-    'id': 1,
-    'name': 'Laurence Reynolds',
-    'tel': '1-657-067-3756 x9782',
-    'address': '187 Kirlin Squares',
-    'opening_hours': '08:00',
-    'description': 'sit est mollitia',
-    'image': 'https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887',
-    'viewCounts': 1,
-    'createdAt': '2019-07-30T16:24:55.432Z',
-    'updatedAt': '2019-07-30T17:26:43.260Z',
-    'CategoryId': 3,
-    'Category': {
-      'id': 3,
-      'name': '義大利料理',
-      'createdAt': '2019-07-30T16:24:55.429Z',
-      'updatedAt': '2019-07-30T16:24:55.429Z'
-    }
-  }
-}
+import AdminRestaurantForm from "../components/AdminRestaurantForm.vue";
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 
 export default {
   components: {
-    AdminRestaurantForm
+    AdminRestaurantForm,
   },
-  data () {
+  data() {
     return {
       restaurant: {
         id: -1,
-        name: '',
-        categoryId: '',
-        tel: '',
-        address: '',
-        description: '',
-        image: '',
-        openingHours: ''
+        name: "",
+        categoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: "",
+      },
+      isProcessing: false
+    };
+  },
+  methods: {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await adminAPI.restaurants.getDetail({ restaurantId });
+
+        const {
+          id,
+          name,
+          tel,
+          address,
+          opening_hours: openingHours,
+          description,
+          image,
+          CategoryId: categoryId,
+        } = data.restaurant;
+
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image,
+          categoryId,
+        };
+      } catch (error) {
+        this.isProcessing = false
+        console.log(error);
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
+    },
+    async handleAfterSubmit (formData) {
+      try {
+        this.isProcessing = true
+        const { data } = await adminAPI.restaurants.update({
+          restaurantId: this.restaurant.id,
+          formData
+        })
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法更新餐廳資料，請稍後再試'
+        })
       }
     }
   },
-  methods: {
-    fetchRestaurant (restaurantId) {
-      console.log('fetchRestaurant id:', restaurantId)
-
-      const { restaurant } = dummyData
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryId: restaurant.CategoryId,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours
-      }
-    },
-    handleAfterSubmit (formData) {
-      for (let [name, value] of formData.entries()) {
-        console.log('Edit | ' + name + ':' + value)
-      }
-    },
+  created() {
+    const { id } = this.$route.params;
+    this.fetchRestaurant(id);
   },
-  created () {
-    const { id } = this.$route.params
+  beforeRouteUpdate (to, from, next) {
+    const { id } = to.params
     this.fetchRestaurant(id)
+    next()
   }
-}
+};
 </script>
